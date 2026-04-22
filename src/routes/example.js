b2467c329262252a3e8214e733fad0907492fa46
chotai <chotai@users.noreply.github.com>
2026-04-22 17:09:16 +0100

    CORE-1111 Remove Mongo from template

--- src/routes/example.js
+++ src/routes/example.js
@@ -1,25 +1,17 @@
-import Boom from '@hapi/boom'
-import { findAllExampleData, findExampleData } from '#/services/ExampleFind.js'
-
 export const example = [
   {
     method: 'GET',
     path: '/example',
-    handler: async (request, h) => {
-      const entities = await findAllExampleData(request.db)
+    handler: (request, h) => {
+      const entities = [{ entityId: '1' }, { entityId: '2' }]
       return h.response(entities)
     }
   },
   {
     method: 'GET',
     path: '/example/{exampleId}',
-    handler: async (request, h) => {
-      const entity = await findExampleData(request.db, request.params.exampleId)
-
-      if (!entity) {
-        return Boom.notFound()
-      }
-
+    handler: (request, h) => {
+      const entity = { entityId: '1' }
       return h.response(entity)
     }
   }
